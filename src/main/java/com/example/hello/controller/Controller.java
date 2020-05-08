2c5594083313a29a8c25cd74a99bea1dbfe9871f
Firman Budi Safrizal <firmanbudis@gmail.com>
2020-05-09 00:35:45 +0700

    add ip host

--- src/main/java/com/example/hello/controller/Controller.java
+++ src/main/java/com/example/hello/controller/Controller.java
@@ -2,11 +2,21 @@ package com.example.hello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
 
 @RestController("/")
 public class Controller {
     @GetMapping
     public String hello(){
-        return "hello from server 1";
+        InetAddress ip = null;
+        try{
+            ip = InetAddress.getLocalHost();
+        } catch (UnknownHostException e){
+            e.printStackTrace();
+        }
+
+        return "hello from server 2 - "+ ip;
     }
 }
