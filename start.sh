81c4d22d3c8f5838a1d579fc48c878c0538bcd0c
Firman Budi Safrizal <firmanbudis@gmail.com>
2020-05-13 03:39:50 +0700

    add tmp how to run in start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,5 @@
 #java -cp app:app/lib/* com.example.hello.HelloApplication
 java -jar /app/app.jar &
 envoy -c /etc/service-envoy.yaml --service-cluster service${SERVICE_NAME}
+#java -cp /app/resources:/app/classes:/app/libs/* com.example.hello.HelloApplication
+#docker run -it -v service-envoy.yaml:/etc/service-envoy.yaml --entrypoint /bin/sh frmnjn/hello start.sh
